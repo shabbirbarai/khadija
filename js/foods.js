547352bd835f03251922934a70e8a4556d7f70aa
shabbir <shabbirbarai@gmail.com>
2017-06-10 04:54:25 -0700

    fullwebsite

---
+++ js/foods.js
@@ -0,0 +1,55 @@
+var items = [{
+    id: "031",
+    name: 'Tea',
+    image: 'tea3.jpg',
+    price:100,
+    quantity: 0
+}, {
+    id: "032",
+    name: 'Biryani',
+    image: '003.jpg',
+    price:180,    
+    quantity: 0
+
+}, {
+    id: "033",
+    name: 'Soup',
+    image: '33.jpeg',
+    price:150,
+    quantity: 0
+
+}, {
+    id: "034",
+    name: 'Burger',
+    image: 'BurgerDining.jpg',
+    price:175,
+    quantity: 0
+
+}, {
+    id: "035",
+    name: 'Pizza',
+    image: 'big-dipper.png',
+    price:495,
+    quantity: 0
+
+}, {
+    id: "036",
+    name: 'irani Boti ',
+    image: '006.jpg',
+    price:295,
+    quantity: 0
+
+}, {
+    id: "037",
+    name: 'snwich',
+    image: 'images10000009.jpg',
+    price:220,
+    quantity: 0
+}, {
+    id: "038",
+    name: 'Chollu Kabab ',
+    image: '007.jpg',
+    price:200,
+    quantity: 0
+
+}];
